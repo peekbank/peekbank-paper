7e7c98f10ba9e4446542bb8e9a10e3378235b155
Mike Frank <mcfrank@stanford.edu>
2021-02-23 20:46:53 -0800

    rts

--- misc/rle-inverse-rle demo.R
+++ misc/rle-inverse-rle demo.R
@@ -1,16 +1,27 @@
-foo <- select(aoi_data_joined, administration_id, trial_id, t_norm, aoi) 
+load(file = "data/aoi_data_joined.Rds")
+
+foo <- select(aoi_data_joined, administration_id, trial_id, t_norm, aoi) %>%
+  arrange(administration_id, trial_id)
+
 rled <- foo %>%
   group_by(administration_id, trial_id) %>%
   summarise(lengths = rle(aoi)$lengths, 
-            values = rle(aoi)$values) 
+            values = rle(aoi)$values, 
+            t_norm_min = min(t_norm)) 
 
-tic()
+profvis::profvis({
+  
 unrled <- rled %>%
-  group_by(administration_id, trial_id) %>%
+  group_by(administration_id, trial_id, t_norm_min) %>%
   nest() %>%
   mutate(rle_vector = map(data,
-                           ~ `class<-`(list(lengths = .$lengths, values = .$values), "rle")), 
-            inverse_vector = map(rle_vector,inverse.rle)) %>%
+                           ~ `class<-`(list(lengths = .$lengths, 
+                                            values = .$values), "rle")), 
+            aoi = map(rle_vector, inverse.rle)) %>%
   select(-data, -rle_vector) %>%
-  unnest(inverse_vector)
-toc()
+  unnest(aoi) %>%
+  group_by(administration_id, trial_id) %>%
+  mutate(t_norm = seq(t_norm_min[1], t_norm_min[1] + (n()-1) * 25, 25)) %>%
+  select(administration_id, trial_id, t_norm, aoi) %>%
+  arrange(administration_id, trial_id)
+})
